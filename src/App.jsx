7609ef5421838f0663959bcab8bb18536a660899
Sten-TõnisFeldmann <sten-tonis.feldmann@voco.ee>
2026-09-02 08:26:38 +0000

    Ülesanne 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,23 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem.jsx'
 
 const App = () => {
+  const expenses = [
+    {
+      date: new Date(2024, 10, 12),
+      title: 'New book',
+      price: 30.99
+    },
+    {
+      date: new Date(2024, 10, 12),
+      title: 'New jeans',
+      price: 99.99
+    }
+  ]
+
   return (
     <div className="App">
-      <ExpenseItem />
-    
+      <ExpenseItem data={expenses[0]}/>
+      <ExpenseItem data={expenses[1]}/>
     </div>
   );
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+
+    console.log(props)
+    console.log(props.data)
+
     return (
         <div className='expense-item'>
-            <div>Date</div>
+            <div>{props.data.date.toString()}</div>
                 <div className='expense-item__description'>
-                    <h2>Title</h2>
-                    <div className='expense-item__price'>Price</div>
+                    <h2>{props.data.title}</h2>
+                    <div className='expense-item__price'>{props.data.price}</div>
                 </div>
             </div>
     );
