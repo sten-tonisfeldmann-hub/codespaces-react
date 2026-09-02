8c0a5b4dbbf4ebade5e71c7413299a91e189cd0a
Sten-TõnisFeldmann <sten-tonis.feldmann@voco.ee>
2026-09-02 08:42:29 +0000

    Ülesanne 4

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,18 +1,16 @@
+import ExpenseDate from './ExpenseDate.jsx'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
 
-    console.log(props)
-    console.log(props.data)
-
     return (
         <div className='expense-item'>
-            <div>{props.data.date.toString()}</div>
+                <ExpenseDate date={props.data.date}/>
                 <div className='expense-item__description'>
                     <h2>{props.data.title}</h2>
                     <div className='expense-item__price'>{props.data.price}</div>
-                </div>
             </div>
+        </div>
     );
 }
 
