e4f31a755574a21ea67227c921aa51bae8de4b21
Sten-TõnisFeldmann <sten-tonis.feldmann@voco.ee>
2026-09-21 08:07:23 +0000

    Ülesanne 12

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem.jsx';
+import ExpensesFilter from './ExpensesFilter.jsx';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2024');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses = props.items.filter(
+        (expense) => expense.date.getFullYear().toString() === filteredYear
+    );
+
     return (
         <div className='expenses'>
-            {props.items.map((expense) => (
+            <ExpensesFilter
+                selected={filteredYear}
+                onChangeFilter={filterChangeHandler}
+            />
+            {filteredExpenses.map((expense) => (
                 <ExpenseItem key={expense.title} data={expense} />
             ))}
         </div>
